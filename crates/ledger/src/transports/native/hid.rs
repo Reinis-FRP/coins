d835b047e863d577ee11a585b6d79b9857af26e2
Reinis Martinsons <reinis@umaproject.org>
2025-06-10 09:57:18 +0000

    fix: remove debug
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- crates/ledger/src/transports/native/hid.rs
+++ crates/ledger/src/transports/native/hid.rs
@@ -8,7 +8,6 @@ use crate::{
 use byteorder::{BigEndian, ReadBytesExt};
 use hidapi_rusb::{DeviceInfo, HidApi, HidDevice};
 use once_cell::sync::Lazy;
-use tracing::debug;
 use std::{
     io::Cursor,
     sync::{Mutex, MutexGuard},
@@ -62,7 +61,6 @@ fn first_ledger(api: &HidApi) -> Result<HidDevice, NativeTransportError> {
         .next()
         .ok_or(NativeTransportError::DeviceNotFound)?;
 
-    debug!("Opening first ledger device: {:?}", device);
     open_device(api, device)
 }
 
